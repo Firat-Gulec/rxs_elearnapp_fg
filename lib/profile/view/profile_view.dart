503158f1059f7bdccc704635a9fa003482d5dbff
Fırat-Gulec <firat_gulec@hotmail.com>
2022-09-13 17:15:45 +0300

    profile_view state created

--- lib/profile/view/profile_view.dart
+++ lib/profile/view/profile_view.dart
@@ -3,11 +3,15 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:provider/provider.dart';
+import '../../core/Init/cache/cache_manager.dart';
 import '../../core/Init/provider/theme_provider.dart';
+import '../../core/constants.dart';
 import '../../core/widget/appbar_widget.dart';
 import '../../core/widget/button_widget.dart';
 import '../../core/widget/profile_widget.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import '../../core/widget/textfield_widget.dart';
+import 'package:rxs_elearnapp_fg/core/Init/auth/auth_require_state.dart';
 
 
 class ProfileView extends StatefulWidget {
@@ -17,9 +21,93 @@ class ProfileView extends StatefulWidget {
   State<ProfileView> createState() => _ProfileViewState();
 }
 
-class _ProfileViewState extends State<ProfileView> {
+class _ProfileViewState extends AuthRequiredState<ProfileView> with CacheManager {
   Future<void> _changeTheme() async {
     setState(() {});
+  }
+
+   final _usernameController = TextEditingController();
+  final _websiteController = TextEditingController();
+  var _loading = false;
+
+  /// Called once a user id is received within `onAuthenticated()`
+  Future<void> _getProfile(String userId) async {
+    setState(() {
+      _loading = true;
+    });
+    final response = await supabase
+        .from('profiles')
+        .select()
+        .eq('id', userId)
+        .single()
+        .execute();
+    final error = response.error;
+    if (error != null && response.status != 406) {
+      context.showErrorSnackBar(message: error.message);
+    }
+    final data = response.data;
+    if (data != null) {
+      _usernameController.text = (data['username'] ?? '') as String;
+      _websiteController.text = (data['user_role'] ?? '') as String;
+    }
+    setState(() {
+      _loading = false;
+    });
+  }
+
+  /// Called when user taps `Update` button
+  Future<void> _updateProfile() async {
+    setState(() {
+      _loading = true;
+    });
+
+  
+  
+
+    final userName = _usernameController.text;
+    final website = _websiteController.text;
+    final user = supabase.auth.currentUser;
+    print(user);
+    final updates = {
+      'id': user!.id,
+      'username': userName,
+      'user_role': website,
+      'updated_at': DateTime.now().toIso8601String(),
+    };
+    final response = await supabase.from('profiles').upsert(updates).execute();
+    final error = response.error;
+    if (error != null) {
+      context.showErrorSnackBar(message: error.message);
+    } else {
+      context.showSnackBar(message: 'Successfully updated profile!');
+    }
+    setState(() {
+      _loading = false;
+    });
+  }
+
+  Future<void> _signOut() async {
+    final response = await supabase.auth.signOut();
+    clearCache();
+    final error = response.error;
+    if (error != null) {
+      context.showErrorSnackBar(message: error.message);
+    }
+  }
+
+  @override
+  void onAuthenticated(Session session) {
+    final user = session.user;
+    if (user != null) {
+      _getProfile(user.id);
+    }
+  }
+
+  @override
+  void dispose() {
+    _usernameController.dispose();
+    _websiteController.dispose();
+    super.dispose();
   }
 
   @override
